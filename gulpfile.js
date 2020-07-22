4cb1285c061a05a6470edf9f2ecb646a92acbcf6
hedaira <mezzo666@mail.ru>
2020-07-22 12:08:38 +0300

    //

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ function compSvg() {
 function compSvgList() {
     return gulp
         .src('svg/**/*.svg')
-        .pipe(fileList('fileList.json', { flatten: true }))
+        .pipe(fileList('fileList.json', { flatten: true, removeExtensions: true }))
         .pipe(gulp.dest('icons'));
 }
 
