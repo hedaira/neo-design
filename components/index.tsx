601ecbe80e5db8c07bea0048fd94a61ee042ae4d
hedaira <mezzo666@mail.ru>
2020-09-05 22:28:10 +0300

    icon NeoTypography

--- components/index.tsx
+++ components/index.tsx
@@ -17,3 +17,5 @@ export { default as NeoBar } from './bar'
 export { default as NeoSwitch } from './switch'
 export { default as NeoTag } from './tag'
 
+export { default as NeoTypography } from './typography'
+
