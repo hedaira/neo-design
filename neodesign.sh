f2f2f567a5ff36bf8c02e41e3de11e9a4b160162
hedaira <mezzo666@mail.ru>
2020-12-22 18:26:18 +0300

    fix version && run "npm dedupe"

--- neodesign.sh
+++ neodesign.sh
@@ -2,7 +2,7 @@
 SERVICE_NAME=neodesign
 PID_PATH_NAME=/opt/neodesign/neodesign-pid
 
-SYS_PARAMS="-Xmx8g -Dlogging.file=/opt/neodesign/logs/neodesign.log -Dlogging.level.root=info"
+SYS_PARAMS="-Xmx8g -Dlogging.file=/opt/neo-design/logs/neodesign.log -Dlogging.level.root=info"
 case $1 in
 install)
        echo "Starting $SERVICE_NAME ..."
@@ -25,7 +25,7 @@ gulp)
 start)
        echo "Starting $SERVICE_NAME ..."
   if [ ! -f $PID_PATH_NAME ]; then
-       nohup npm run storybook 1>>/opt/neodesign/logs/neodesign.log 2>>/opt/neodesign/logs/neodesign.log & echo $! > $PID_PATH_NAME
+       nohup npm run storybook 1>>/opt/neo-design/logs/neodesign.log 2>>/opt/neo-design/logs/neodesign.log & echo $! > $PID_PATH_NAME
        echo "$SERVICE_NAME started ..."
   else
        echo "$SERVICE_NAME is already running ..."
