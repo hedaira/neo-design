2114ebb8615c2cb05425a8a1fc7a5b0b01d951b3
unk774 <nwmfmm@gmail.com>
2021-05-31 11:21:22 +0400

    https://jira-new.neoflex.ru/browse/NFDGNRSDEV-448 paragraph 3

--- components/_utils/Title.tsx
+++ components/_utils/Title.tsx
@@ -17,34 +17,14 @@ interface Props {
 export function Title(props:Props) {
     const width = props.width ? parseInt(props.width, 10) : inputElementStandardWidth;
     const typographyWidth = 150;
-    const contentContainer = useRef(null);
-    const [contentWidth,setContentWidth] = useState(0);
-    const [observer, setObserver] = useState(new MutationObserver(() => {
-        if (contentContainer.current) {
-            setContentWidth(contentContainer.current.offsetWidth)
-        }
-    }));
-
-    useEffect(()=>{
-        if (!width) {
-            observer.observe(document, {subtree: true, childList: true});
-            return ()=>{
-                observer.disconnect()
-            }
-        }
-    },[]);
-
-    const divContainer = <div ref={contentContainer} style={{width:"fit-content"}}>
-        {props.children}
-    </div>;
 
     if (props.titleOrientation === "Left" || props.titleOrientation === "Right") {
         return <NeoRow hidden={props.hidden} className={"neo-title-container"} style={{minWidth: typographyWidth + 80 + "px", maxWidth: width + 150, display: "flex", alignItems: "flexStart"}}>
             <NeoCol span={props.titleOrientation === "Left" ? 6 : 18}>
-                {props.titleOrientation === "Left" ? <NeoTypography style={{padding: "8px 12px 4px 0", wordBreak: "break-word", textAlign: "right"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography> : divContainer}
+                {props.titleOrientation === "Left" ? <NeoTypography style={{padding: "8px 12px 4px 0", wordBreak: "break-word", textAlign: "right"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography> : props.children}
             </NeoCol>
             <NeoCol span={props.titleOrientation === "Right" ? 6 : 18}>
-                {props.titleOrientation === "Right" ? <NeoTypography style={{padding: "4px 0 4px 12px", wordBreak: "break-word"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography> : divContainer}
+                {props.titleOrientation === "Right" ? <NeoTypography style={{padding: "4px 0 4px 12px", wordBreak: "break-word"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography> : props.children}
             </NeoCol>
         </NeoRow>
     }
@@ -52,8 +32,8 @@ export function Title(props:Props) {
     if (props.titleOrientation === "Top" || props.titleOrientation === "Bottom") {
         return <NeoRow hidden={props.hidden}>
             <NeoCol style={{width: "fit-content"}}>
-                {props.titleOrientation === "Top" ? <NeoTypography style={{width: width ? width : contentWidth, wordBreak: "break-word", paddingBottom: "4px", textAlign: "center"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography> : divContainer}
-                {props.titleOrientation === "Top" ? divContainer : <NeoTypography style={{width: width ? width : contentWidth, wordBreak: "break-word", paddingBottom: "4px", textAlign: "center"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography>}
+                {props.titleOrientation === "Top" ? <NeoTypography style={{width: width , wordBreak: "break-word", paddingBottom: "4px", textAlign: "center"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography> : props.children}
+                {props.titleOrientation === "Top" ? props.children : <NeoTypography style={{width: width , wordBreak: "break-word", paddingBottom: "4px", textAlign: "center"}}>{props.title}{props.required&&<span style={{color:'#ad1457'}}> *</span>}</NeoTypography>}
             </NeoCol>
         </NeoRow>
     }
