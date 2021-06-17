7a025cf9c659de3e285a8f72af717b0e6056c839
itrushchelev <freedrex@gmail.com>
2021-06-17 12:21:20 +0300

    fix import

--- components/steps/steps.tsx
+++ components/steps/steps.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import {Steps} from 'antd';
 import {Component} from "react";
-import {NeoTypography} from "../../lib";
+import {NeoTypography} from "../index";
 
 
 export interface StepsProps {
